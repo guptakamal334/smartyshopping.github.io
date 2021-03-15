2336d4e78524500b24336a120f029e7def3ecd1b
Kamal Gupta <guptakamal334@gmail.com>
2021-03-15 13:23:00 +0530

    okk

--- Smarty_Shopping_E-Commerce/UserSmartyShopping/order.php
+++ Smarty_Shopping_E-Commerce/UserSmartyShopping/order.php
@@ -1,7 +1,15 @@
-<?php
-        require('header.php');
+<?php 
+require('header.php');
+if(!isset($_SESSION['USER_LOGIN'])){
+    // header("location:index.php");
+	?>
+	<script>
+	window.location.href='index.php';
+	</script>
+	<?php
+}
+?>
 
-   ?>
 
 <div class="path">
         <a href="index.php">Home &nbsp;&nbsp;/</a> 
@@ -11,7 +19,7 @@
    
   <!-- -----------------------------order page---------------------- -->
 <div class="small-container cart-page">
-    <table>
+    <table class="order_page">
         <tr>
             <th>ORDER ID</th>
             <th>ORDER DATE</th>
@@ -20,9 +28,21 @@
             <TH>PAYMENT STATUS</TH>
             <TH>ORDER STATUS</TH>
         </tr>       
+        <?php
+            $uid=$_SESSION['USER_ID'];
+            $res=mysqli_query($con,"select * from `order` where user_id='$uid'");
+            
+            while($row=mysqli_fetch_assoc($res)){
+        ?>
         <tr>
-            <td></td>
+            <a href="order_detail.php?id=<?php echo $row['id'];?>"><td><?php echo $row['id'];?></td>
+            <td><?php echo $row['added_on'];?></td>
+            <td><?php echo $row['address'].",".$row['city'].",".$row['state'].",".$row['pincode'];?></td>
+            <td><?php echo $row['payment_type'];?></td>
+            <td><?php echo $row['payment_status'];?></td>
+            <td><?php echo $row['order_status'];?></td></a>
         </tr>
+        <?php } ?>
     </table>
 </div>
 
