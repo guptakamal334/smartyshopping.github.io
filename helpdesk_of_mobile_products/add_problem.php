8f89c30ccb4bb611de2fc5b3977fcae71b63e831
Kamal Gupta <guptakamal334@gmail.com>
2021-04-14 13:17:07 +0530

    ok

--- helpdesk_of_mobile_products/add_problem.php
+++ helpdesk_of_mobile_products/add_problem.php
@@ -2,6 +2,10 @@
 <?php
         require('header.php');
         $product_id=get_safe_value($con,$_GET['product_id']);
+        $sql="select product.*, customer.* from product,customer where product.customer_id=customer.customer_id and product.product_id='$product_id'";
+        $res=mysqli_query($con,$sql);
+        $count=mysqli_num_rows($res);
+       
 ?>
 
   <!-- -------------------------path seciton----------------------------- -->
@@ -13,18 +17,45 @@
    <!-- -----------------------------Add Problem ------------------------ -->
    <div class="login add_prob" >
      <h2 class="title">ADD PROBLEM</h2>
-     <div class="row">
+     <div class="problem_form">
+       <?php 
+           if ($count>0){
+           $row=mysqli_fetch_assoc($res);
+       ?>
         <label for="">Product Name</label>
-        <span>fder</span>
+        <span><?php echo $row['product_name'];?></span>
         <label for="">IMEI Number</label>
-        <span>fder</span>
+        <span><?php echo $row['imei_no'];?></span>
         <label for="">Model Number</label>
-        <span>fder</span>
-        
+        <span><?php echo $row['model_no'];?></span>
+        <br>
         <label for="">Customer Name</label>
-        <span>fder</span>
-        <label for="">Expire Date</label>
-        <span>fder</span> 
+        <span><?php echo $row['first_name'].$row['last_name']; ?></span>
+        <label for="">Purchase Date</label>
+        <span><?php echo $row['date_of_purchase'];?></span> 
+        <label for="">Warranty Expire Date</label>
+        <span><?php echo $row['expire_date'];?></span> 
+        <br>
+        <label for="">Problem Title</label>
+        <span><select name="" id="">
+          <option value="">kamal</option>
+        </select></span>
+        <label for="">Problem Category</label>
+        <span><select name="" id="">
+          <option value="">kamal</option>
+          <option value="">kamal</option>
+          <option value="">kamal</option>
+        </select></span>  <br>
+        <label for="">Problem Description</label>
+      
+          <textarea name="" id="" cols="30" rows="5"></textarea>
+  
+          <input type="submit" value="Submit">
+          <input type="reset" value="Reset">
+          <?php }else{
+             header('location:home.php');
+             die();
+          }?>
     </div>
   </div>
 <?php
