f2ee4ec4e745f419299eecb6b39ee8098e62bc39
Kamal Gupta <guptakamal334@gmail.com>
2021-04-16 14:05:47 +0530

    okk

--- helpdesk_of_mobile_products/update_status.php
+++ helpdesk_of_mobile_products/update_status.php
@@ -9,11 +9,16 @@
           $count=mysqli_num_rows($res);
           if($count>0){
             $row=mysqli_fetch_assoc($res);
+            $request_no=$row['request_no'];
             $product_id=$row['product_id'];
             $product_name=$row['product_name'];
             $imei_no=$row['imei_no'];
             $model_no=$row['model_no'];
-            $customer_name= $row['first_name'].$row['last_name'];
+            $customer_name= $row['title']." ".$row['first_name']." ".$row['last_name'];
+            $problem_title=$row['name'];
+            $problem_category=$row['cat_name'];
+            $description=$row['problem_description'];
+            $curr_date=$row['curr_time'];
             $date_of_purchase=$row['date_of_purchase'];
             $expire_date=$row['expire_date'];
           }else{
@@ -23,18 +28,15 @@
       }
 
         if(isset($_GET['submit'])){
-          $product_id=get_safe_value($con,$_GET['product_id']);
-          $problem_title=get_safe_value($con,$_GET['problem_tit']);
-          $problem_category=get_safe_value($con,$_GET['problem_cat']);
-          $description=get_safe_value($con,$_GET['description']);
-          $status=1;
-          $request_no="REQ".rand(10000,100000);
+          $request_no=get_safe_value($con,$_GET['request_no']);
+          $problem_status=get_safe_value($con,$_GET['problem_status']);
+         echo '<script>alert("jfdfdf")</script>' ;
           date_default_timezone_set('Asia/Kolkata');
           $currentTime = date( 'Y-m-d H:i:s');
           
-          mysqli_query($con,"insert into problem (product_id,problem_title_id,problem_category_id,problem_description,problem_status_id,curr_time,request_no) values ('$product_id','$problem_title','$problem_category','$description','$status','$currentTime','$request_no')");
+          // mysqli_query($con,"insert into problem (product_id,problem_title_id,problem_category_id,problem_description,problem_status_id,curr_time,request_no) values ('$product_id','$problem_title','$problem_category','$description','$status','$currentTime','$request_no')");
 
-          echo "your Request has been added Successfull and your Request Number is ".$request_no;
+          // echo "your Request has been added Successfull and your Request Number is ".$request_no;
 
           
         }
@@ -53,20 +55,31 @@
      <h2 class="title">UPDATE STATUS</h2>
      <div class="problem_form"> 
      <form action="" method="get">
+        <label for="">Request Number</label>
+        <span name='name'><?php echo $request_no;?></span>
         <label for="">Product Name</label>
         <span name='name'><?php echo $product_name;?></span>
         <label for="">IMEI Number</label>
-        <input class="product_id" type="number" name="product_id" value="<?php echo $product_id;?>">
-        <span name="imei_no" value="<?php echo $imei_no;?>"><?php echo $imei_no;?></span>
+        <input class="product_id" type="number" name="request_no" value="<?php echo $request_no;?>">
+        <span><?php echo $imei_no;?></span>
         <label for="">Model Number</label>
         <span><?php echo $model_no;?></span>
-        <br>
+        
         <label for="">Customer Name</label>
         <span><?php echo $customer_name; ?></span>
         <label for="">Purchase Date</label>
         <span><?php echo $date_of_purchase;?></span> 
         <label for="">Warranty Expire Date</label>
         <span><?php echo $expire_date;?></span> 
+
+        <label for="">Problem Title</label>
+        <span><?php echo $problem_title; ?></span>
+        <label for="">Porblem Category</label>
+        <span><?php echo $problem_category;?></span> 
+        <label for="">Added Problem Date</label>
+        <span><?php echo $curr_date;?></span> 
+        <label for="">Description</label>
+        <span><?php echo $description;?></span> 
         <br>
         
         <label for="">Status</label>
@@ -83,10 +96,6 @@
         </span>
         
           <br> 
-        <label for="">Problem Description</label>
-      
-        <textarea name="description" id="" cols="30" rows="5" required></textarea>
-
           <input type="submit" value="Submit" name="submit">
           <input type="reset" value="Reset">
          
